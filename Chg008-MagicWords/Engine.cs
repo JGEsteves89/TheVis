77f83c77fa40614cced8679ae54b376e920fe07d
José Esteves <jose.esteves@gmail.com>
2019-01-07 12:00:26 +0100

    Ch008-MagicWords done

---
+++ Chg008-MagicWords/Engine.cs
@@ -0,0 +1,83 @@
+﻿using Engine;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using System.Windows.Forms;
+using OpenTK.Input;
+using System.Drawing.Drawing2D;
+
+namespace Chg008_MagicWords {
+
+    public class Engine : IEngine {
+        public static float WIDTH = 800;
+        public static float HEIGHT = 800;
+        public static int OUTPUT_SIZE = 6;
+        public static Vector2 GRAVITY = new Vector2(0,-0.05f);
+        public static List<Vector2> TARGETS = GetPointsOfString("Tarina", 200f, new Vector2(70, 200));
+
+        public Vector2 target = new Vector2(WIDTH / 2, HEIGHT / 2);
+        public Comet comet = new Comet(new Vector2(-20, HEIGHT));
+
+        public override void SetUp() {
+            base.SetUp();
+            Canvas((int)WIDTH, (int)HEIGHT);
+            BackgroundColor(51);
+
+            comet.vec = new Vector2(1f, 5f);
+            comet.radius = 5;
+            comet.target = target;
+            comet.targetActive = true;
+        }
+        public override void Draw() {
+            base.Draw();
+            comet.Update();
+            comet.Draw();
+        }
+
+        public static List<Vector2> GetPointsOfString(string text, float size,Vector2 trans) {
+            int minD = 1;
+            List<Vector2> points = new List<Vector2>();
+            Vector2 first = new Vector2();
+            Bitmap bmp = new Bitmap((int)WIDTH,(int) HEIGHT);
+            GraphicsPath gp = new GraphicsPath();
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (Font f = new Font("Arial", size)) {
+                gp.AddString(text, f.FontFamily, 0, size, new Point(0, 0), StringFormat.GenericDefault);
+                first = new Vector2(gp.PathPoints[0].X, -gp.PathPoints[0].Y);
+                for (int i = 0; i < gp.PathPoints.Length - 1; i++) {
+                    if (gp.PathTypes[i + 1] != 0) {
+                        Vector2 p1 = new Vector2(gp.PathPoints[i].X, -gp.PathPoints[i].Y);
+                        Vector2 p2 = new Vector2(gp.PathPoints[i + 1].X, -gp.PathPoints[i + 1].Y);
+
+                        points.Add(p1);
+                        points.AddRange(GetPointsBetween(minD, p1, p2));
+                        points.Add(p2);
+                    } else {
+                        Vector2 last = new Vector2(gp.PathPoints[i].X, -gp.PathPoints[i].Y);
+                        points.AddRange(GetPointsBetween(minD, last, first));
+                        first = new Vector2(gp.PathPoints[i + 1].X, -gp.PathPoints[i + 1].Y);
+                    }
+                }
+
+            }
+            for (int i = 0; i < points.Count; i++) {
+                points[i] += trans;
+            }
+            return points;
+        }
+        public static List<Vector2> GetPointsBetween(int minD, Vector2 p1, Vector2 p2) {
+            List<Vector2> points = new List<Vector2>();
+
+            Vector2 dir = p2 - p1;
+            int segments = (int)dir.Length / minD;
+            for (int n = 1; n < segments - 1; n++) {
+                points.Add(p1 + dir.Normalized() * minD * n);
+            }
+            return points;
+        }
+    }
+}
---
+++ Chg008-MagicWords/Objects.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using Engine;
+using static Engine.IEngine;
+using System.Drawing;
+
+
+namespace Chg008_MagicWords {
+    public class Particle {
+        public float limitSpeed = 5;
+        public Vector2 pos;
+        public Vector2 vec;
+        public Vector2 acc;
+        public float radius = 4;
+        public float dumpEffect = 0.2f;
+        public Color color = Color.White;
+        public long timeSpan = 0;
+        public Vector2 target;
+        public bool targetActive = false;
+        
+        public Particle(Vector2 pos, Vector2 vec, Vector2 acc) {
+            this.pos = pos;
+            this.vec = vec;
+            this.acc = acc;
+        }
+        public Particle(Vector2 pos) : this(pos, new Vector2(0, 0), new Vector2(0, 0)) {
+        }
+        public Particle(Vector2 pos, Vector2 vec) : this(pos, vec, new Vector2(0, 0)) {
+        }
+        public virtual void Update() {
+            if (targetActive) {
+                Vector2 dir = (target - pos).Normalized();
+                acc = dumpEffect * dir;
+            }
+
+            pos += vec;
+            vec += acc;
+
+            if (vec.Length> limitSpeed) 
+                vec = vec.Normalized() * limitSpeed;
+        }
+        public virtual void Draw() {
+            SetColor(color);
+            timeSpan++;
+            DrawCircle(pos.X, pos.Y, radius);
+        }
+    }
+    public class Comet : Particle {
+        List<Particle> tail = new List<Particle>();
+        public int delay = 200;
+        public int count = 0;
+        public Comet(Vector2 pos) : base(pos) {
+
+        }
+        public override void Draw() {
+            base.Draw();
+            int maxTime = 200;
+            foreach (Particle item in tail) {
+                int color = Mapi((int)item.timeSpan, 0, maxTime, 0, 255);
+                item.color = Color.FromArgb(255 - color, 255 - color, 255);
+                item.Draw();
+            }
+        }
+        public override void Update() {
+            base.Update();
+
+            if (Engine.TARGETS.Count != 0) {
+                int number = (int)Math.Min(Engine.TARGETS.Count, Engine.OUTPUT_SIZE);
+                for (int i = 0; i < number; i++) {
+                    Vector2 newVel = -vec;
+                    newVel.X += Rndf(-2f, 2f);
+                    newVel.Y += Rndf(-2f, 2f);
+
+                    Particle newPart = new Particle(pos, newVel, Engine.GRAVITY);
+                    newPart.target = Engine.TARGETS[0];
+                    newPart.targetActive = true;
+                    Engine.TARGETS.RemoveAt(0);
+                    tail.Add(newPart);
+                }
+            } else {
+                target = new Vector2(2 * Engine.WIDTH, 2 * Engine.HEIGHT);
+                if (count != delay) {
+                    count++;
+                } 
+                //else {
+                //    for (int i = tail.Count - 1; i >= 0; i--) {
+                //        tail[i].targetActive = false;
+                //        tail[i].acc = new Vector2(0, 0);
+
+                //        Vector2 dir = (tail[i].target - tail[i].pos);
+                //        float mag = (float)Math.Min(dir.Length, 5f);
+                //        if (mag > 0.1f)
+                //            tail[i].vec = mag * dir.Normalized();
+                //        else {
+                //            tail[i].vec = new Vector2(0, 0);
+                //            tail[i].pos = tail[i].target;
+                //        }
+
+                //    }
+                //}
+            }
+            for (int i = tail.Count - 1; i >= 0; i--) {
+                float m = 1.73f;
+                float r = 0.00158f;
+                float x = tail[i].timeSpan*2;
+                float exp = m * ((float)Math.Exp(r*x)-1f);
+                tail[i].dumpEffect = Truncf(exp, 0, 50);
+                if (tail[i].pos.X < -Engine.WIDTH) tail.RemoveAt(i);
+                else if (tail[i].pos.X > 2 * Engine.WIDTH) tail.RemoveAt(i);
+                else if (tail[i].pos.Y < -Engine.HEIGHT) tail.RemoveAt(i);
+                else if (tail[i].pos.Y > 2 * Engine.HEIGHT) tail.RemoveAt(i);
+                else tail[i].Update();
+            }
+        }
+    }
+}
