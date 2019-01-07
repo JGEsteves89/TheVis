56e5f0d46219349af502eb2b29acf6a0f4d5a1fc
José Esteves <jose.esteves@gmail.com>
2019-01-07 20:23:43 +0100

    Working using a mapped random color per radius

--- Chg009-CircledSign/Engine.cs
+++ Chg009-CircledSign/Engine.cs
@@ -27,15 +27,19 @@ namespace Chg009_CircledSign {
             }
 
             internal void draw() {
-                SetFill(Color.White);
+                if (!colorMap.ContainsKey((int) radius)) {
+                    colorMap.Add((int)radius, Rndc());
+                }
+                SetFill(colorMap[(int)radius]);
                 DrawCircle(pos.X, pos.Y, radius);
             }
         }
 
-        public static float WIDTH = 800;
+        public static float WIDTH = 2400;
         public static float HEIGHT = 800;
         public List<Circle> circles = new List<Circle>();
         public List<Vector2> textPoints = new List<Vector2>();
+        public static Dictionary<int, Color> colorMap = new Dictionary<int, Color>();
         public override void SetUp() {
             base.SetUp();
             Canvas((int)WIDTH, (int)HEIGHT);
@@ -43,17 +47,18 @@ namespace Chg009_CircledSign {
 
             //Create a image of white text on black background
             string text = "Tarina";
-            int textSize = 150;
-            Vector2 textPosition = new Vector2(100, 100);
+            int textSize = 300;
+            Vector2 textPosition = new Vector2(100, HEIGHT-100);
             Vector2 imageSize = new Vector2(WIDTH, HEIGHT);
-            Bitmap img = CreateBWTextImage(text, textSize, textPosition, imageSize);
+            Bitmap img = CreateBWTextImage(text, textSize, textPosition);
 
 
             // Retrieves every point that is on the white part of the image
             for (int x = 0; x < WIDTH; x++) {
                 for (int y = 0; y < HEIGHT; y++) {
-                    if (img.GetPixel(x, y) == Color.White) {
-                        textPoints.Add(new Vector2(x, y));
+                    Color pix = img.GetPixel(x, y);
+                    if (pix.R == 255 && pix.G == 255 && pix.B == 255) {
+                        textPoints.Add(new Vector2(x, (int)HEIGHT - y));
                     }
                 }
             }
@@ -64,20 +69,27 @@ namespace Chg009_CircledSign {
 
         public override void Draw() {
             base.Draw();
+            int number = 10;
+            int count = 0;
 
-            if (textPoints.Count != 0) {
+            while(count != number) {
+                if ((textPoints.Count == 0)) break;
                 // Generates a random point in the image
                 Vector2 newPoint = textPoints[Rndi(textPoints.Count)];
+
+                textPoints.Remove(newPoint);
 
                 // Point must
                 //      Be on the white part of the image
                 //      It cannot be inside of another circle
                 if (!insideCircle(circles, newPoint)) {
-                    // Create a circle on the selected point 
-                    circles.Add(new Circle(newPoint));
+                    Circle newCircle = new Circle(newPoint);
+                    if (!isTouchingOtherCircle(circles, newCircle)) {
+                        // Create a circle on the selected point 
+                        circles.Add(newCircle);
+                        count++;
+                    }
                 }
-
-                textPoints.Remove(newPoint);
             }
 
             foreach (Circle circle in circles) {
@@ -104,7 +116,7 @@ namespace Chg009_CircledSign {
         private bool isTouchingOtherCircle(List<Circle> circles, Circle circle) {
             foreach (Circle item in circles) {
                 if (item == circle) continue;
-                if ((item.pos - circle.pos).Length <= item.radius + circle.radius)
+                if ((item.pos - circle.pos).Length <= item.radius + circle.radius + 2)
                     return true;
             }
             return false;
@@ -112,10 +124,10 @@ namespace Chg009_CircledSign {
 
         private Bitmap CreateBWTextImage(string text, int textSize, Vector2 textPosition) {
             Bitmap bmp = new Bitmap((int)WIDTH, (int)HEIGHT);
-            GraphicsPath gp = new GraphicsPath();
             using (Graphics g = Graphics.FromImage(bmp)) {
                 using (Font f = new Font("Arial", textSize)) {
-                    gp.AddString(text, f.FontFamily, 0, textSize, new Point((int)textPosition.X, (int)textPosition.Y), StringFormat.GenericDefault);
+                    g.FillRectangle(Brushes.Black, new Rectangle(0, 0, (int)WIDTH, (int)HEIGHT));
+                    g.DrawString(text, f, Brushes.White, new Point((int)textPosition.X, (int)HEIGHT-(int)textPosition.Y));
                 }
             }
             return bmp;
